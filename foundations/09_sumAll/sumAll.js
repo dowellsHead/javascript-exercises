2b069e008e97bba8ef56765926067cc2c682a42a
dowellsHead <xkopylova@gmail.com>
2026-03-15 01:00:01 +0300

    finish 10&11 tasks

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a>=0 && b>=0 && typeof a == typeof b && typeof a == typeof 42 && Number.isInteger(a) && Number.isInteger(b)){
+        let start = a<=b ? a : b;
+        let end = a<=b ? b : a;
+        let arr = [start, ];
+        for (i=1;i<=end-start;i++){
+            arr.push(start+i);
+        }
+        let result = arr.reduce((sum, current) => sum + current, 0);
+        return result;
+    } else
+        return("ERROR");
 };
 
 // Do not edit below this line
