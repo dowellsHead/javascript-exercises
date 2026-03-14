95a6c193ad0577213ffaaba652fdacbeb6593096
dowellsHead <xkopylova@gmail.com>
2026-03-14 21:17:03 +0300

    add a "remove from array" function

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elems) {
+    let red_arr = arr;
+    for (let i=elems.length; i>=0; i--){
+        red_arr = red_arr.filter(item => item !== elems[i]);
+    }
+    return red_arr;  
 };
 
 // Do not edit below this line
