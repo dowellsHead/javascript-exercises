487f7c5fa15009828ec830aa299d95de3fd30b5e
dowellsHead <xkopylova@gmail.com>
2026-03-14 20:27:42 +0300

    finish 6th task on repeating a string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num >= 0){
+        let new_str = []
+        while (num >0){
+            new_str=new_str.concat(str);
+            num--;
+        }
+        return new_str.join('');
+    } else if (num<0){
+        return('ERROR')
+    } 
 };
 
 // Do not edit below this line
