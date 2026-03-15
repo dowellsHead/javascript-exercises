3593735b99afdbb3f74f3ce988d6b4f8405a345f
dowellsHead <xkopylova@gmail.com>
2026-03-15 19:27:28 +0300

    finish 11th task

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  deg = Math.round((deg-32)*5/9*10)/10;
+  return deg;  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  deg = Math.round((deg*9/5 + 32)*10)/10;
+  return deg;
 };
 
 // Do not edit below this line
